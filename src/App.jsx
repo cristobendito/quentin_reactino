bf210547aa733f686c9166f2d058987017058c4c
cristobendito <flopybbb@gmail.com>
2024-05-28 16:44:53 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import Footer from './components/footer/footer';  
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-
-    </>
-  )
+    <div className="App">
+      <nav className="navbar">
+        <h1>Navbar</h1>
+      </nav>
+      <main className="main-content">
+        <h2>Contenido Principal</h2>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
