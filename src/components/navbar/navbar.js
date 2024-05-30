6d29293802263a96d2403cf26c4d41954db747b3
Manudies <manudies@gmail.com>
2024-05-30 13:17:17 +0200

    feat: saco la funcion de buscar por géneros a otro archivo

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,35 @@
+import { fetchGenres } from '../../services/api.js';
+
+const genres = await fetchGenres();
+
+const iconGenres = [
+"fas fa-fist-raised",
+"fas fa-hiking",
+"fas fa-film",
+"fas fa-laugh",
+"fas fa-bomb",
+"fas fa-book",
+"fas fa-theater-masks",
+"fas fa-users",
+"fas fa-dragon",
+"fas fa-landmark",
+"fas fa-ghost",
+"fas fa-music",
+"fas fa-search",
+"fas fa-heart",
+"fas fa-rocket",
+"fas fa-tv",
+"fas fa-user-secret",
+"fas fa-bomb",
+"fas fa-hat-cowboy",
+]
+//crear un constante que agregue en orden el icono en className 
+
+const genresIcons = genres.map((genre, index) => {
+    return { id: genre.id, name: genre.name, className: iconGenres[index] };
+})
+
+
+
+
+export {genresIcons};
